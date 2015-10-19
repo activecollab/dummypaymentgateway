7a061d8056f296affe571623d4192e5044f6038e
Ilija Studen <ilija.studen@activecollab.com>
2015-10-19 11:07:41 +0200

    Tested offsite payment gateway

---
+++ test/src/OffsitePaymentGatewayTest.php
@@ -0,0 +1,277 @@
+<?php
+
+namespace ActiveCollab\Payments\Test;
+
+use ActiveCollab\DummyPaymentGateway\OffsitePaymentGateway;
+use ActiveCollab\Payments\Customer\CustomerInterface;
+use ActiveCollab\Payments\Customer\Customer;
+use ActiveCollab\Payments\Dispatcher\DispatcherInterface;
+use ActiveCollab\Payments\Gateway\GatewayInterface;
+use ActiveCollab\Payments\Order\OrderInterface;
+use ActiveCollab\Payments\Order\Order;
+use ActiveCollab\Payments\OrderItem\OrderItem;
+use ActiveCollab\Payments\Order\Refund\RefundInterface;
+use ActiveCollab\Payments\Subscription\Cancelation\CancelationInterface;
+use ActiveCollab\Payments\Subscription\Change\ChangeInterface;
+use ActiveCollab\Payments\Subscription\FailedPayment\FailedPaymentInterface;
+use ActiveCollab\Payments\Subscription\Rebill\RebillInterface;
+use ActiveCollab\Payments\Subscription\Subscription;
+use ActiveCollab\Payments\Subscription\SubscriptionInterface;
+use ActiveCollab\DateValue\DateTimeValue;
+
+/**
+ * @package ActiveCollab\Payments\Test
+ */
+class OffsitePaymentGatewayTest extends TestCase
+{
+    /**
+     * @var OffsitePaymentGateway
+     */
+    protected $gateway;
+
+    /**
+     * @var CustomerInterface
+     */
+    protected $customer;
+
+    /**
+     * @var DateTimeValue
+     */
+    protected $timestamp;
+
+    /**
+     * @var OrderInterface
+     */
+    protected $order;
+
+    /**
+     * @var SubscriptionInterface
+     */
+    protected $subscription;
+
+    /**
+     * Set up test environment
+     */
+    public function setUp()
+    {
+        parent::setUp();
+
+        $this->gateway = new OffsitePaymentGateway($this->dispatcher);
+        $this->customer = new Customer('John Doe', 'john@example.com');
+        $this->timestamp = new DateTimeValue('2015-10-15');
+        $this->order = new Order($this->customer, '2015-01', $this->timestamp, 'USD', 1200, [
+        new OrderItem('Expensive product', 1, 1000),
+        new OrderItem('Not so expensive product', 2, 100),
+        ]);
+
+        $this->subscription = new Subscription($this->customer, '2015-01', $this->timestamp, SubscriptionInterface::MONTHLY, 'USD', 25, [
+        new OrderItem('Monthly SaaS cost', 1, 25),
+        ]);
+    }
+
+    /**
+     * Tear down test environment
+     */
+    public function tearDown()
+    {
+        $this->gateway = $this->customer = $this->timestamp = $this->order = null;
+
+        parent::tearDown();
+    }
+
+    /**
+     * Just verify that tests are running
+     */
+    public function testClassExists()
+    {
+        $this->assertTrue(class_exists(OffsitePaymentGateway::class));
+    }
+
+    /**
+     * Test if order completed triggers an event
+     */
+    public function testOrderCompletedTriggersAnEvent()
+    {
+        $event_triggered = false;
+
+        $this->dispatcher->listen(DispatcherInterface::ON_ORDER_COMPLETED, function(GatewayInterface $gateway, OrderInterface $order) use (&$event_triggered) {
+            $this->assertInstanceOf(OffsitePaymentGateway::class, $gateway);
+            $this->assertInstanceOf(OrderInterface::class, $order);
+
+            $this->assertEquals($this->order->getReference(), $order->getReference());
+
+            $event_triggered = true;
+        });
+
+        $this->gateway->triggerOrderCompleted($this->order);
+        $this->assertTrue($event_triggered);
+    }
+
+    /**
+     * Test if order refund properly triggers an event
+     */
+    public function testOrderRefundedTriggersAnEvent()
+    {
+        $event_triggered = false;
+
+        $this->dispatcher->listen(DispatcherInterface::ON_ORDER_REFUNDED, function(GatewayInterface $gateway, OrderInterface $order, RefundInterface $refund) use (&$event_triggered) {
+            $this->assertInstanceOf(OffsitePaymentGateway::class, $gateway);
+            $this->assertInstanceOf(RefundInterface::class, $refund);
+            $this->assertInstanceOf(OrderInterface::class, $order);
+
+            $this->assertEquals($refund->getOrderReference(), $order->getReference());
+            $this->assertEquals($refund->getTotal(), $order->getTotal());
+
+            $this->assertFalse($refund->isPartial());
+
+            $event_triggered = true;
+        });
+
+        $this->gateway->triggerOrderRefunded($this->order, $this->timestamp);
+        $this->assertTrue($event_triggered);
+    }
+
+    /**
+     * Test if partial order refund properly triggers an event
+     */
+    public function testOrderPartiallyRefundedTriggersAnEvent()
+    {
+        $event_triggered = false;
+
+        $this->dispatcher->listen(DispatcherInterface::ON_ORDER_PARTIALLY_REFUNDED, function(GatewayInterface $gateway, OrderInterface $order, RefundInterface $refund) use (&$event_triggered) {
+            $this->assertInstanceOf(OffsitePaymentGateway::class, $gateway);
+            $this->assertInstanceOf(RefundInterface::class, $refund);
+            $this->assertInstanceOf(OrderInterface::class, $order);
+
+            $this->assertEquals($refund->getOrderReference(), $order->getReference());
+            $this->assertGreaterThan($refund->getTotal(), $order->getTotal());
+
+            $this->assertInternalType('array', $refund->getItems());
+            $this->assertCount(1, $refund->getItems());
+            $this->assertEquals('Expensive product', $refund->getItems()[0]->getDescription());
+
+            $this->assertTrue($refund->isPartial());
+
+            $event_triggered = true;
+        });
+
+        $this->gateway->triggerOrderPartiallyRefunded($this->order, [
+        new OrderItem('Expensive product', 1, 1000),
+        ], $this->timestamp);
+
+        $this->assertTrue($event_triggered);
+    }
+
+    /**
+     * Test if subscription created triggers an event
+     */
+    public function testSubscriptionActivatedTriggersAnEvent()
+    {
+        $event_triggered = false;
+
+        $this->dispatcher->listen(DispatcherInterface::ON_SUBSCRIPTION_ACTIVATED, function(GatewayInterface $gateway, SubscriptionInterface $subscription) use (&$event_triggered) {
+            $this->assertInstanceOf(OffsitePaymentGateway::class, $gateway);
+            $this->assertInstanceOf(Subscription::class, $subscription);
+
+            $this->assertEquals($this->subscription->getReference(), $subscription->getReference());
+
+            $event_triggered = true;
+        });
+
+        $this->gateway->triggerSubscriptionActivated($this->subscription);
+
+        $this->assertTrue($event_triggered);
+    }
+
+    /**
+     * Test if subscription rebill triggers an event
+     */
+    public function testSubscriptionRebillTriggersAnEvent()
+    {
+        $event_triggered = false;
+
+        $this->dispatcher->listen(DispatcherInterface::ON_SUBSCRIPTION_REBILL, function(GatewayInterface $gateway, SubscriptionInterface $subscription, RebillInterface $rebill) use (&$event_triggered) {
+            $this->assertInstanceOf(OffsitePaymentGateway::class, $gateway);
+            $this->assertInstanceOf(Subscription::class, $subscription);
+            $this->assertInstanceOf(RebillInterface::class, $rebill);
+
+            $this->assertEquals($this->subscription->getReference(), $subscription->getReference());
+            $this->assertEquals($this->subscription->getReference(), $rebill->getSubscriptionReference());
+
+            $event_triggered = true;
+        });
+
+        $this->gateway->triggerSubscriptionRebill($this->subscription);
+
+        $this->assertTrue($event_triggered);
+    }
+
+    /**
+     * Test if subscription rebill triggers an event
+     */
+    public function testSubscriptionChangeTriggersAnEvent()
+    {
+        $event_triggered = false;
+
+        $this->dispatcher->listen(DispatcherInterface::ON_SUBSCRIPTION_CHANGED, function(GatewayInterface $gateway, SubscriptionInterface $subscription, ChangeInterface $change) use (&$event_triggered) {
+            $this->assertInstanceOf(OffsitePaymentGateway::class, $gateway);
+            $this->assertInstanceOf(Subscription::class, $subscription);
+            $this->assertInstanceOf(ChangeInterface::class, $change);
+
+            $this->assertEquals($this->subscription->getReference(), $subscription->getReference());
+            $this->assertEquals($this->subscription->getReference(), $change->getSubscriptionReference());
+
+            $event_triggered = true;
+        });
+
+        $this->gateway->triggerSubscriptionChange($this->subscription);
+
+        $this->assertTrue($event_triggered);
+    }
+
+    /**
+     * Test if subscription cancelation triggers an event
+     */
+    public function testSubscriptionCanceledTriggersAnEvent()
+    {
+        $event_triggered = false;
+
+        $this->dispatcher->listen(DispatcherInterface::ON_SUBSCRIPTION_DEACTIVATED, function(GatewayInterface $gateway, SubscriptionInterface $subscription, CancelationInterface $cancelation) use (&$event_triggered) {
+            $this->assertInstanceOf(OffsitePaymentGateway::class, $gateway);
+            $this->assertInstanceOf(Subscription::class, $subscription);
+            $this->assertInstanceOf(CancelationInterface::class, $cancelation);
+
+            $this->assertEquals($this->subscription->getReference(), $subscription->getReference());
+            $this->assertEquals($this->subscription->getReference(), $cancelation->getSubscriptionReference());
+
+            $event_triggered = true;
+        });
+
+        $this->gateway->triggerSubscriptionDeactivated($this->subscription);
+
+        $this->assertTrue($event_triggered);
+    }
+
+    /**
+     * Test if failed subscription payment triggers an event
+     */
+    public function testFailedSubscriptionPaymentTriggersAnEvent()
+    {
+        $event_triggered = false;
+
+        $this->dispatcher->listen(DispatcherInterface::ON_SUBSCRIPTION_PAYMENT_FAILED, function(GatewayInterface $gateway, SubscriptionInterface $subscription, FailedPaymentInterface $failed_payment) use (&$event_triggered) {
+            $this->assertInstanceOf(OffsitePaymentGateway::class, $gateway);
+            $this->assertInstanceOf(Subscription::class, $subscription);
+            $this->assertInstanceOf(FailedPaymentInterface::class, $failed_payment);
+
+            $this->assertEquals($this->subscription->getReference(), $subscription->getReference());
+            $this->assertEquals($this->subscription->getReference(), $failed_payment->getSubscriptionReference());
+
+            $event_triggered = true;
+        });
+
+        $this->gateway->triggerSubscriptionFailedPayment($this->subscription);
+
+        $this->assertTrue($event_triggered);
+    }
+}
---
+++ test/src/TestCase.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace ActiveCollab\DummyPaymentGateway\Test;
+
+/**
+ * @package ActiveCollab\Payments\Test
+ */
+abstract class TestCase extends \PHPUnit_Framework_TestCase
+{
+}
