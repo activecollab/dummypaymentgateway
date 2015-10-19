3d8eda5f7923c577c061e6fa7a8c7a892b252c76
Ilija Studen <ilija.studen@activecollab.com>
2015-10-19 10:58:50 +0200

    Added Payments package
    Changelogs summary:
    
     - activecollab/payments installed in version dev-master

---
+++ src/ExampleOffsiteGateway.php
@@ -0,0 +1,238 @@
+<?php
+
+namespace ActiveCollab\DummyPaymentGateway;
+
+use ActiveCollab\DateValue\DateTimeValue;
+use ActiveCollab\Payments\Dispatcher\DispatcherInterface;
+use ActiveCollab\Payments\Gateway\Gateway;
+use ActiveCollab\Payments\Order\OrderInterface;
+use ActiveCollab\Payments\Order\Refund\RefundInterface;
+use ActiveCollab\Payments\Order\Refund\Refund;
+use ActiveCollab\Payments\OrderItem\OrderItemInterface;
+use ActiveCollab\DateValue\DateTimeValueInterface;
+use ActiveCollab\Payments\Subscription\Cancelation\Cancelation;
+use ActiveCollab\Payments\Subscription\Change\Change;
+use ActiveCollab\Payments\Subscription\FailedPayment\FailedPayment;
+use ActiveCollab\Payments\Subscription\Rebill\Rebill;
+use ActiveCollab\Payments\Subscription\SubscriptionInterface;
+use ActiveCollab\Payments\Subscription\Cancelation\CancelationInterface;
+use InvalidArgumentException;
+
+/**
+ * @package ActiveCollab\Payments\Test\Fixtures
+ */
+class OffsitePaymentGateway extends Gateway
+{
+    /**
+     * @var OrderInterface[]
+     */
+    private $orders = [];
+
+    /**
+     * @var RefundInterface[]
+     */
+    private $refunds = [];
+
+    /**
+     * @var SubscriptionInterface[]
+     */
+    private $subscriptions = [];
+
+    /**
+     * @var CancelationInterface[]
+     */
+    private $cancelations = [];
+
+    /**
+     * @param \ActiveCollab\Payments\Dispatcher\DispatcherInterface $dispatcher
+     */
+    public function __construct(DispatcherInterface &$dispatcher)
+    {
+        $this->setDispatcher($dispatcher);
+    }
+
+    /**
+     * Return order by order ID
+     *
+     * @param  string         $reference
+     * @return OrderInterface
+     */
+    public function getOrderByReference($reference)
+    {
+        if (isset($this->orders[$reference])) {
+            return $this->orders[$reference];
+        }
+
+        throw new InvalidArgumentException("Order #{$reference} not found");
+    }
+
+    /**
+     * Return refund by refund ID
+     *
+     * @param  string          $refund_reference
+     * @return RefundInterface
+     */
+    public function getRefundByReference($refund_reference)
+    {
+        if (isset($this->refunds[$refund_reference])) {
+            return $this->refunds[$refund_reference];
+        }
+
+        throw new InvalidArgumentException("Refund #{$refund_reference} not found");
+    }
+
+    /**
+     * Trigger product order completed
+     *
+     * @param OrderInterface $order
+     */
+    public function triggerOrderCompleted(OrderInterface $order)
+    {
+        $this->orders[$order->getReference()] = $order;
+
+        $this->getDispatcher()->triggerOrderCompleted($this, $order);
+    }
+
+    /**
+     * Trigger order has been fully refunded
+     *
+     * @param OrderInterface              $order
+     * @param DateTimeValueInterface|null $timestamp
+     */
+    public function triggerOrderRefunded(OrderInterface $order, DateTimeValueInterface $timestamp = null)
+    {
+        $this->orders[$order->getReference()] = $order;
+
+        if (empty($timestamp)) {
+            $timestamp = new DateTimeValue();
+        }
+
+        $refund = new Refund($order->getReference() . '-X', $order->getReference(), $timestamp, $order->getTotal());
+        $refund->setGateway($this);
+
+        $this->refunds[$refund->getReference()] = $refund;
+
+        $this->getDispatcher()->triggerOrderRefunded($this, $order, $refund);
+    }
+
+    /**
+     * Trigger order has been partially refunded
+     *
+     * @param OrderInterface              $order
+     * @param OrderItemInterface[]        $items
+     * @param DateTimeValueInterface|null $timestamp
+     */
+    public function triggerOrderPartiallyRefunded(OrderInterface $order, array $items = null, DateTimeValueInterface $timestamp = null)
+    {
+        $this->orders[$order->getReference()] = $order;
+
+        if (empty($timestamp)) {
+            $timestamp = new DateTimeValue();
+        }
+
+        $refund = new Refund($order->getReference() . '-X', $order->getReference(), $timestamp, 200);
+        $refund->setGateway($this);
+
+        if (!empty($items)) {
+            $refund->setItems($items);
+        }
+
+        $this->refunds[$refund->getReference()] = $refund;
+
+        $this->getDispatcher()->triggerOrderPartiallyRefunded($this, $order, $refund);
+    }
+
+    /**
+     * Trigger subscription activated (created) event
+     *
+     * @param SubscriptionInterface $subscription
+     */
+    public function triggerSubscriptionActivated(SubscriptionInterface $subscription)
+    {
+        $this->subscriptions[$subscription->getReference()] = $subscription;
+
+        $this->getDispatcher()->triggerSubscriptionActivated($this, $subscription);
+    }
+
+    /**
+     * Trigger subscription failed payment event
+     *
+     * @param SubscriptionInterface       $subscription
+     * @param DateTimeValueInterface|null $timestamp
+     */
+    public function triggerSubscriptionRebill(SubscriptionInterface $subscription, DateTimeValueInterface $timestamp = null)
+    {
+        $this->subscriptions[$subscription->getReference()] = $subscription;
+
+        if (empty($timestamp)) {
+            $timestamp = new DateTimeValue();
+        }
+
+        $rebill = new Rebill($subscription->getReference(), $timestamp);
+        $rebill->setGateway($this);
+
+        $this->getDispatcher()->triggerSubscriptionRebill($this, $subscription, $rebill);
+    }
+
+    /**
+     * Trigger subscription failed payment event
+     *
+     * @param SubscriptionInterface       $subscription
+     * @param DateTimeValueInterface|null $timestamp
+     */
+    public function triggerSubscriptionChange(SubscriptionInterface $subscription, DateTimeValueInterface $timestamp = null)
+    {
+        $this->subscriptions[$subscription->getReference()] = $subscription;
+
+        if (empty($timestamp)) {
+            $timestamp = new DateTimeValue();
+        }
+
+        $change = new Change($subscription->getReference(), $timestamp);
+        $change->setGateway($this);
+
+        $this->getDispatcher()->triggerSubscriptionChanged($this, $subscription, $change);
+    }
+
+    /**
+     * Trigger subscription deactivated (canceled) event
+     *
+     * @param SubscriptionInterface       $subscription
+     * @param DateTimeValueInterface|null $timestamp
+     */
+    public function triggerSubscriptionDeactivated(SubscriptionInterface $subscription, DateTimeValueInterface $timestamp = null)
+    {
+        $this->subscriptions[$subscription->getReference()] = $subscription;
+
+        if (empty($timestamp)) {
+            $timestamp = new DateTimeValue();
+        }
+
+        $cancelation = new Cancelation($subscription->getReference() . '-X', $subscription->getReference(), $timestamp);
+        $cancelation->setGateway($this);
+
+        $this->cancelations[$cancelation->getReference()] = $cancelation;
+
+        $this->getDispatcher()->triggerSubscriptionDeactivated($this, $subscription, $cancelation);
+    }
+
+    /**
+     * Trigger subscription failed payment event
+     *
+     * @param SubscriptionInterface       $subscription
+     * @param DateTimeValueInterface|null $timestamp
+     */
+    public function triggerSubscriptionFailedPayment(SubscriptionInterface $subscription, DateTimeValueInterface $timestamp = null)
+    {
+        $this->subscriptions[$subscription->getReference()] = $subscription;
+
+        if (empty($timestamp)) {
+            $timestamp = new DateTimeValue();
+        }
+
+        $failed_payment = new FailedPayment($subscription->getReference(), $timestamp);
+        $failed_payment->setGateway($this);
+
+        $this->getDispatcher()->triggerSubscriptionPaymentFailed($this, $subscription, $failed_payment);
+    }
+}
